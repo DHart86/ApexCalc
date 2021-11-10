348891cc4c2b3522c200c4ca852294cd78339f23
DHart86 <djhartley86@yahoo.com>
2021-11-10 00:26:01 -0500

    Cleanup
    Debug variant day

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
 $( document ).ready(function() {
 
 
-/*$(".calcButton").on("click", function() {
-  weight = $("#firstNumber").val();
-  bfper = $("#secondNumber").val();
-
-  // RADIO
-  var radios = $('[name="blkcut"]');
-  var goal;
-  for (var i in radios) {
-    if (radios[i].checked) {
-      goal = radios[i].value;
-    }
-  }
-
-  //ACTIVITY LEVEL
-  function tdee(weigh, per) {
-    level = $("#acLev").val();
-    return (((((100 - per) / 100) * weigh * 9.79759519) + 370) * level) * goal;
-  }
-  $("#result").text(Math.round(tdee(weight, bfper)) + " calories");
-
-  //PROTEIN
-  function prot(weigh, per) {
-    return (((100 - per) / 100) * weigh);
-  }
-  $("#protein").text("Protein: " + Math.round(prot(weight, bfper)));
-
-  //FAT
-  function fat(weigh, per) {
-    return (((100 - per) / 100) * weigh * 0.4);
-  }
-  $("#fats").text("Fat: " + Math.round(fat(weight, bfper)));
-})*/
 
 
-function calcCals() {
+$(".calcButton").on("click", function() {
   weight = $("#firstNumber").val();
   bfper = $("#secondNumber").val();
 
@@ -91,13 +59,7 @@ function calcCals() {
   $("#or").text("OR");
   $("#smallday").text("5 days of " + Math.round(smallDay(weight, bfper)) + " calories");
   $("#bigday").text("2 days of " + Math.round(bigDay(weight, bfper)) + " calories");
-
-
-}
-
-
-
-
+});
 
 
 });
